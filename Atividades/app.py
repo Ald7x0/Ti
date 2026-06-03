c55031ef773d3abb30dbc1f32f83088901695805
Ald7x0 <senacatividades07@gmail.com>
2026-06-03 11:03:05 -0300

    Add files via upload

--- Atividades/app.py
+++ Atividades/app.py
@@ -1,26 +1,51 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+alunos = []
+
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html', alunos=alunos)
 
-@app.route("/inscricao", methods=["POST"])
+@app.route('/cadastrar', methods=['GET', 'POST'])
+def cadastrar():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        idade = request.form['idade']
+        curso = request.form['curso']
+        email = request.form['email']
 
-def inscricao():
-    nickname = request.form.get("nickname")
-    jogo = request.form.get("jogo")
-    email = request.form.get("email")
+        if nome and idade and curso and email:
+            alunos.append({
+                'nome': nome,
+                'idade': idade,
+                'curso': curso,
+                'email': email
+            })
 
-    if not nickname or not jogo or not email:
-        mensagem = "Preencha todos os campos obrigatórios."
-    elif len(nickname) < 4:
-        mensagem = "Preencha todos os campos obrigatórios."
-    else:
-        mensagem = "Inscrição realizada com sucesso!"
+        return redirect(url_for('index'))
 
-    return render_template("resultado.html", mensagem=mensagem)
+    return render_template('cadastrar.html')
 
-if __name__ == "__main__":
+@app.route('/editar/<int:id>', methods=['GET', 'POST'])
+def editar(id):
+    aluno = alunos[id]
+
+    if request.method == 'POST':
+        aluno['nome'] = request.form['nome']
+        aluno['idade'] = request.form['idade']
+        aluno['curso'] = request.form['curso']
+        aluno['email'] = request.form['email']
+
+        return redirect(url_for('index'))
+
+    return render_template('editar.html', aluno=aluno, id=id)
+
+@app.route('/excluir/<int:id>')
+def excluir(id):
+    alunos.pop(id)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
     app.run(debug=True)
